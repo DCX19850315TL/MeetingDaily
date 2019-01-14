2ff9f3cde8b1901ef344e570ae59092ecd6b1389
tanglei <tanglei_0315@163.com>
2019-01-14 11:49:55 +0800

    Initial commit

---
+++ MeetingDaily.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+'''
+@author: tanglei
+@contact: tanglei_0315@163.com
+@file: MeetingDaily.py
+@time: 2019/1/14 11:24
+'''
---
+++ common/GetMeetingInfo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+'''
+@author: tanglei
+@contact: tanglei_0315@163.com
+@file: GetMeetingInfo.py
+@time: 2019/1/14 11:26
+'''
+#根据配置文件获取指定时间点的会议信息
