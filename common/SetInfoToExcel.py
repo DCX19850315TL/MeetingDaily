b41a0c9f2adfbd4e75d8fd616f37c768c3ccf957
tangl <tanglei_0315@163.com>
2019-05-17 15:21:32 +0800

    修改excel报表显示文字

--- common/SetInfoToExcel.py
+++ common/SetInfoToExcel.py
@@ -43,7 +43,7 @@ def SetInfoToExcel():
     MeetingLen = MeetingInfo.List_len()
     if os.path.isfile(excel_file):
         shutil.move(excel_file,excel_backup_file)
-    Header = {"A1":"序号","B1":"会议号","C1":"会议时间","D1":"持续时间","E1":"端到端总体合格率","F1":"平均合格率","G1":"端到端最高合格率","H1":"端到端最低合格率","I1":"端到端|合格状态|丢包率|空音包","J1":"开会方数","K1":"用户列表","L1":"设备类型"}
+    Header = {"A1":"序号","B1":"会议号","C1":"会议时间","D1":"持续时间","E1":"端到端总体合格率","F1":"平均合格率","G1":"端到端最高合格率","H1":"端到端最低合格率","I1":"端到端|合格状态|丢包合格率|空音包合格率","J1":"开会方数","K1":"用户列表","L1":"设备类型"}
     wb = Workbook()
     ws = wb.active
     for k,v in Header.items():
@@ -55,11 +55,11 @@ def SetInfoToExcel():
                 "C%d" % (item + 2): MeetingInfo.GetMeetingTime()[item],
                 "D%d" % (item + 2): MeetingInfo.GetMeetingDuration()[item],
                 "E%d" % (item + 2): MeetingInfo.GetPtoPTotal()[item],
-                "F%d" % (item + 2): "丢包:%s\n空音包:%s" % (
+                "F%d" % (item + 2): "丢包合格率:%s\n空音包合格率:%s" % (
             MeetingInfo.PacketLossAverage()[item], MeetingInfo.SoundPacketLossAverage()[item]),
-                "G%d" % (item + 2): "丢包:%s\n空音包:%s" % (
+                "G%d" % (item + 2): "丢包合格率:%s\n空音包合格率:%s" % (
                 MeetingInfo.PacketLossMax()[item], MeetingInfo.SoundPacketLossMax()[item]),
-                "H%d" % (item + 2): "丢包:%s\n空音包:%s" % (
+                "H%d" % (item + 2): "丢包合格率:%s\n空音包合格率:%s" % (
                 MeetingInfo.PacketLossMin()[item], MeetingInfo.SoundPacketLossMin()[item]),
                 "I%d" % (item + 2): "\n".join(MeetingInfo.DetailData()[item]),
                 "J%d" % (item + 2): MeetingInfo.MeetingPeopleNumber()[item],
